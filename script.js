50215c6467b5ba152a5b0d478e0ee45a8dcb21b3
piyush <hellopiyush.lab@gmail.com>
2026-03-11 06:26:56 +0000

    winning conditions satisfied

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// players will be objects
-// a player has the following
-//      - name
-//      - marker
-// we need nothing more for a player, and there seems to be no need of privacy here.
-// hence, can make it normal object
-
-// for players only, i will use a constructor
-
+// Constructor for making players, used only twice
 function createPlayer (name, marker) {
     if(!new.target) {
         console.log("createPlayer is a constructor");
@@ -19,24 +11,17 @@ function createPlayer (name, marker) {
 const playerOne = new createPlayer("Player One", "O");
 const playerTwo = new createPlayer("Player Two", "X");
 
-// choose first player
-// change array value
-// check if the current player is a winner
-// change the player
-// change array value
-// check if the current player is a winner
-// this will be the gameplay loop
-
-// to make sure the array is not out in open,
-// we will use immediately invoked function expression
-// return an object of methods to a gameplay variable
-// and use the functions from there
-
+// immediately invoked function expression
+// for all the game related functions
 const gameplay = (() => {
-    // the array for the storing the X and O
-    // giving it an additional value of all 1s
-    let tictactoeArray = [1,1,1,1,1,1,1,1,1];
+
+    // array to store the moves
+    // let tictactoeArray = [1,1,1,1,1,1,1,1,1];
+    let tictactoeArray = [];
+    
+    // variable for current player
     let currentPlayer;
+
     // function that starts the game
     // this function will be called the moment page is loaded
     const gameStart = (playerOne, playerTwo) => {
@@ -44,9 +29,20 @@ const gameplay = (() => {
         currentPlayer = playerOne;
         console.log("game started");
     }
-    // this function is called everytime a move is made
+
+    // function to make a move
     const makeMove = (choice) => {
-        tictactoeArray[choice-1] = currentPlayer.marker;
+        tictactoeArray[choice] = currentPlayer.marker;
+
+        // check winner before changing player
+        let resultOfCheckWinner = gameplay.checkWinner(choice);
+        if (resultOfCheckWinner) {
+            gameLoop = false;
+            console.log("someone won, idk who tho")
+        }
+
+        // if there is a winner, loop would've broken
+        // otherwise,
         // change the player
         if (currentPlayer === playerOne) {
             currentPlayer = playerTwo;
@@ -55,19 +51,70 @@ const gameplay = (() => {
         }
         console.log(tictactoeArray);
     }
-    const checkWinner = () => {
+    const checkWinner = (choice) => {
         // code to check if there is a winner
-        let someoneWon = true;
 
-        // if there is a winner, break the gameloop
-        if (someoneWon = true) {
-            gameLoop = false;
+        let rows        = [ [0,1,2], 
+                            [3,4,5], 
+                            [6,7,8] ];
+        let columns     = [ [0,3,6], 
+                            [1,4,7], 
+                            [2,5,8] ];
+        let diagnols    = [ [0,4,8],
+                            [2,4,6] ];
+
+        // finding the position of choice in the arrays
+        positionOfChoiceInRows =        gameplay.linearSearch(rows, choice); // [0,0]
+        positionOfChoiceInColumns =     gameplay.linearSearch(columns, choice);
+        positionOfChoiceInDiagnols =    gameplay.linearSearch(diagnols, choice);
+
+        // check winning condition within rows
+        if (positionOfChoiceInRows[0] !== -1) {
+            if (
+                tictactoeArray[rows[positionOfChoiceInRows[0]][0]] === tictactoeArray[rows[positionOfChoiceInRows[0]][1]]
+                &&
+                tictactoeArray[rows[positionOfChoiceInRows[0]][0]] === tictactoeArray[rows[positionOfChoiceInRows[0]][2]]
+            ) {
+                return true;
+            } 
         }
+        // check winning condition within columns
+        if (positionOfChoiceInColumns[0] !== -1) {
+            if (
+                tictactoeArray[columns[positionOfChoiceInColumns[0]][0]] === tictactoeArray[columns[positionOfChoiceInColumns[0]][1]]
+                &&
+                tictactoeArray[columns[positionOfChoiceInColumns[0]][0]] === tictactoeArray[columns[positionOfChoiceInColumns[0]][2]]
+            ) {
+                return true;
+            }
+        }
+        // check within condition within diagnols 
+        if (positionOfChoiceInDiagnols[0] !== -1) {
+            if (
+                tictactoeArray[diagnols[positionOfChoiceInDiagnols[0]][0]] === tictactoeArray[diagnols[positionOfChoiceInDiagnols[0]][1]]
+                &&
+                tictactoeArray[diagnols[positionOfChoiceInDiagnols[0]][0]] === tictactoeArray[diagnols[positionOfChoiceInDiagnols[0]][2]]
+            ) {
+                return true;
+            } 
+        }
+    }
+    // this linear search algorithm will find the location of the choice in the winning condition arrays
+    const linearSearch = (array, choice) => {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if (array[i][j] == choice) {
+                    return [i, j];
+                }
+            }
+        }
+        return [-1, -1];
     }
     return {
         gameStart,
         makeMove,
-        checkWinner
+        checkWinner,
+        linearSearch
     }
 })()
 
@@ -76,5 +123,4 @@ let gameLoop = true;
 while (gameLoop === true) {
     let choice = prompt("choose a number between 1 and 10");
     gameplay.makeMove(choice);
-    gameplay.checkWinner();
 }
